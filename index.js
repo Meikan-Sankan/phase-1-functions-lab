023d32f34ebe554b95493cefb3d37f318bd27c57
Meikan-Sankan <162958820+Meikan-Sankan@users.noreply.github.com>
2024-04-15 16:56:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(someValue - 42); 
+}
+
+
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264; 
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264; ch block is 264 feet
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0; 
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02; 
+  } else if (distance > 2000 && distance < 2500) {
+    return 25; 
+  } else {
+    return 'cannot travel that far'; 
+  }
+}
+
+module.exports = {
+  distanceFromHqInBlocks,
+  distanceFromHqInFeet,
+  distanceTravelledInFeet,
+  calculatesFarePrice
+};
+r solution in this file!
